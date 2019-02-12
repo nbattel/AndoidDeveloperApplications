e225d00294a9350fdad54df82f27c313dfbdfe6c
nbattel <nbattel@uwo.ca>
2019-02-12 02:37:42 -0500

    Completed Connect 3 Game

--- Connect3Game/app/src/main/java/com/myappcompany/nick/connect3game/MainActivity.java
+++ Connect3Game/app/src/main/java/com/myappcompany/nick/connect3game/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,51 +20,78 @@ public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0;
 
+    boolean gameActive = true;
+
     public void dropIn(View view)
     {
         ImageView counter = (ImageView) view;
 
-        Log.i("Tag", counter.getTag().toString());
-
         //Getting the tag as a string from the object and parsing it to an int to later be stored in the array
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        //Setting each element in the array to it's corresponding coloured game object
-        gameState[tappedCounter] = activePlayer;
-
-        counter.setTranslationY(-1500);
-
-        if(activePlayer == 0)
+        if(gameState[tappedCounter] == 2 && gameActive)
         {
-            counter.setImageResource(R.drawable.yellow);
-            activePlayer = 1;
-        }
-        else
-        {
-            counter.setImageResource((R.drawable.red));
-            activePlayer = 0;
-        }
 
-        counter.animate().translationYBy(1500).rotation(3600).setDuration(100);
+            //Setting each element in the array to it's corresponding coloured game object
+            gameState[tappedCounter] = activePlayer;
 
-        for (int[] winningPosiiton : winningPositions)
-        {
-            if (gameState[winningPosiiton[0]] == gameState[winningPosiiton[1]] && gameState[winningPosiiton[1]] == gameState[winningPosiiton[2]] && gameState[winningPosiiton[0]] != 2)
-            {
+            counter.setTranslationY(-1500);
 
-                String winner = "";
-                if(activePlayer == 1)
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource((R.drawable.red));
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2)
                 {
-                    winner = "Yellow";
+
+                    gameActive = false;
+                    String winner = "";
+
+                    if (activePlayer == 1) {
+                        winner = "Yellow";
+                    } else {
+                        winner = "Red";
+                    }
+
+                    Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+                    TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+                    winnerTextView.setText(winner + " has Won!");
+                    playAgainButton.setVisibility(view.VISIBLE);
+                    winnerTextView.setVisibility(view.VISIBLE);
                 }
-                else
-                {
-                   winner = "Red";
-                }
-                Toast.makeText(this, winner + " has Won!", Toast.LENGTH_LONG).show();
             }
         }
     }
+
+    public void playAgain(View view)
+    {
+        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+        playAgainButton.setVisibility(view.INVISIBLE);
+        winnerTextView.setVisibility(view.INVISIBLE);
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+        for (int i = 0; i < gridLayout.getChildCount(); i++)
+        {
+            ImageView counter = (ImageView) gridLayout.getChildAt(i);
+            counter.setImageDrawable(null);
+        }
+
+        for(int i = 0; i < gameState.length; i++)
+        {
+            gameState[i] = 2;
+        }
+        activePlayer = 0;
+        gameActive = true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
