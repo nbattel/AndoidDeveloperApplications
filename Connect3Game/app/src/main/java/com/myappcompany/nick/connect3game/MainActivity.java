633e423af44da883c6fee86bf2a4e6a5a12b28af
nbattel <nbattel@uwo.ca>
2019-02-13 13:00:02 -0500

    Updated to display Tie Games

--- Connect3Game/app/src/main/java/com/myappcompany/nick/connect3game/MainActivity.java
+++ Connect3Game/app/src/main/java/com/myappcompany/nick/connect3game/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
     int activePlayer = 0;
-
+    int score = 0;
     boolean gameActive = true;
 
     public void dropIn(View view)
@@ -39,9 +39,11 @@ public class MainActivity extends AppCompatActivity {
 
             if (activePlayer == 0) {
                 counter.setImageResource(R.drawable.yellow);
+                score++;
                 activePlayer = 1;
             } else {
                 counter.setImageResource((R.drawable.red));
+                score++;
                 activePlayer = 0;
             }
 
@@ -63,6 +65,14 @@ public class MainActivity extends AppCompatActivity {
                     Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
                     TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
                     winnerTextView.setText(winner + " has Won!");
+                    playAgainButton.setVisibility(view.VISIBLE);
+                    winnerTextView.setVisibility(view.VISIBLE);
+                }
+                else if(score == 9)
+                {
+                    Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+                    TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+                    winnerTextView.setText("Tie Game!");
                     playAgainButton.setVisibility(view.VISIBLE);
                     winnerTextView.setVisibility(view.VISIBLE);
                 }
